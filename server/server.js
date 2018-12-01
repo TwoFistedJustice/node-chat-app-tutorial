b129c90c6170ca5d32aa28f6f5e2918f5286edfa
TwoFistedJustice <phaltak-1@yahoo.com>
2018-11-30 17:26:38 -0800

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,11 +1,30 @@
+const path = require('path'); //node module which normalizes convoluted paths
 const express = require('express');
-const path = require('path');
+const http = require('http'); // required to use socket.io with express
+const socketIO = require('socket.io');
 
-const app = express();
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
+var app = express();
+
+// normally you pass in a req-res callback, but you can just pass in app here
+// Now we are using the http server instead of  the express server
+// so we need to change app.listen to server.listen
+var server = http.createServer(app); // will be passed into socketIO()
+var io = socketIO(server);
+
 
 app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+  // console.log(socket);
+  console.log('New user connected -- server.js io.on()');
+  
+  socket.on('disconnect', () => {
+    console.log('Disonnected from client -- server.js io.on()');
+  })
+});
 
 
 console.log('__dirname', __dirname);
@@ -13,6 +32,6 @@ console.log('old way: ', __dirname + '/../public');
 console.log('node path module way:', publicPath);
 
 
-app.listen(port, () => {
+server .listen(port, () => {
   console.log(`Listening on port ${port}`);
 });

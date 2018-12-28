92670861c3c811a35694706e192ce212be42c43b
TwoFistedJustice <phaltak-1@yahoo.com>
2018-12-28 13:43:21 -0800

    Create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ var app = express ();
 var server = http.createServer (app); // will be passed into socketIO()
 var io = socketIO (server);
 
+const {generateMessage} = require('./utils/message');
 
 app.use (express.static (publicPath));
 
@@ -22,40 +23,49 @@ io.on ('connection', (socket) => {
   console.log ('New user connected -- server.js io.on()');
   
   
-  socket.emit ('newEmail', {
-    from: 'joe@example.com',
-    text: 'a cuppa joe in the morning',
-    createdAt: 123
-  });
+  // socket.emit ('newEmail', {
+  //   from: 'joe@example.com',
+  //   text: 'a cuppa joe in the morning',
+  //   createdAt: 123
+  // });
+  //
   
-  socket.emit ('newMessage', {
-    from: 'larry@larrysusedcars.com',
-    text: 'It is better to have loved and lost than to have financed a used car.',
-    createdAt: 123
-  });
+  socket.emit('newEmail', generateMessage('joe@example.com', 'a cuppa joe in the morning'));
+  
+  // socket.emit ('newMessage', {
+  //   from: 'larry@larrysusedcars.com',
+  //   text: 'It is better to have loved and lost than to have financed a used car.',
+  //   createdAt: 123
+  // });
+  socket.emit('newMessage', generateMessage('larry@larrysusedcars.com', 'It is better to have loved and lost than to have financed a used car.' ));
   
   
   // this listens for a new message from index.html, then broadcasts it , via io.emit to all connections
   socket.on ('createMessage', (message) => {
     console.log ('createMessage:', message);
-    io.emit ('newMessage', {
-      from: message.from,
-      text: message.text,
-      createAt: new Date().getTime()
-    });
+    // io.emit ('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createAt: new Date().getTime()
+    // });
+    
+    io.emit('createMessage', generateMessage(message.from, message.text));
+    
+    // socket.emit('newMessage', {
+    //   from: 'Darth Admin',
+    //   text: 'Welcome to the Sith Chat App',
+    //   createAt: new Date().getTime()
+    // });
+  socket.emit('newMessage', generateMessage('Darth Admin', 'Welcome to the Sith Chat App'));
     
     
-    socket.emit('newMessage', {
-      from: 'Darth Admin',
-      text: 'Welcome to the Sith Chat App',
-      createAt: new Date().getTime()
-    });
+    
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   createAt: new Date().getTime()
+    // });
 
-    socket.broadcast.emit('newMessage', {
-      from: message.from,
-      createAt: new Date().getTime()
-    });
-
+    socket.broadcast.emit('newMessage', generateMessage(message.from, message.text));
     
     // .broadcast() emits to every user except the one broadcasting
       // socket.broadcast.emit('newMessage', {

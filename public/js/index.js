8bfd8f398d22e84f18e4807fc553a185b0f6f212
TwoFistedJustice <phaltak-1@yahoo.com>
2019-01-17 15:49:36 -0800

    Make rooms list load the same everywhere

--- public/js/index.js
+++ public/js/index.js
@@ -5,49 +5,20 @@ $ (document).ready (function () {
   // decorate the dropdown when disabled
   // or change the text value to "uncheck box to select"
   var listPopulate;
-  var roomConstants = {
-    room1: "General chat",
-    room2: "Node course chat",
-    room3: "Not safe for work"
-  };
-  // var list = ["General chat", "Node course chat", "Not safe for work"];
-  var listOfRooms = [];
-  
-  var dropdown = $ ("#room-drop-down");
   
   var dropdown = $ ('#room-drop-down');
   var roomTextBox = $ ('#room-text');
   var checkBox = $ ('#because-its-easier-thats-why');
   
-  
   listPopulate = function (rooms) {
-    $ (dropdown).append ($ ("<option / >").val (roomConstants.room1).text (roomConstants.room1));
-    $ (dropdown).append ($ ("<option / >").val (roomConstants.room2).text (roomConstants.room2));
-    $ (dropdown).append ($ ("<option / >").val (roomConstants.room3).text (roomConstants.room3));
-    
-    // TODO - only if rooms len is greater than zero
-    for (var i = 0; i < rooms.length; i++) {
-      if (rooms[i] !== roomConstants.room1 &&
-        rooms[i] !== roomConstants.room2 &&
-        rooms[i] !== roomConstants.room3) {
-        $ (dropdown).append ($ ("<option / >").val (rooms[i]).text (rooms[i]));
+      for (var i = 0; i < rooms.length; i++) {
+        $(dropdown).append ($("<option />").val (rooms[i]).text (rooms[i]));
       }
-      
-    }
-    
   };
-  
-  
-  // for (var i =0; i < listOfRooms.length; i++){
-  //   $(dropdown).append($("<option / >").val(listOfRooms[i]).text(listOfRooms[i]));
-  // }
-  //
-  
   
   checkBox.change (function () {
     
     if ((checkBox).is (':checked')) {
-      
       roomTextBox.prop ('disabled', false);
       dropdown.prop ('disabled', true);
     } else {
@@ -57,27 +28,18 @@ $ (document).ready (function () {
   })
   
   roomTextBox.click (function () {
-    
     roomTextBox.prop ('name', "room");
     dropdown.prop ('name', false);
   });
   
-  
   dropdown.click (function () {
-    
     dropdown.prop ('name', "room");
     roomTextBox.prop ('name', false);
-    
   });
-  
   
   socket.on ('newConnection', function (roomsArray) {
-    // if arr len is greater than zero
-    // check each element to see if it is already in list
-    // if it is NOT, add it
-    listPopulate (roomsArray)
+    listPopulate(roomsArray)
   });
-  
   
 });
 
--- server/utils/rooms.js
+++ server/utils/rooms.js
@@ -4,22 +4,32 @@
 // each "room" has a list of sockets, which are socket.id (user id)
 // if a "room" is actually a user, it will have itself listed as socket
 // if a "room" is actually a room, it will NOT have itself listed as a socket
-var getRoomsList = (rooms) => {
-  var roomsList = [];
-  
-  for(room in rooms) {
-    if(!rooms[room].sockets.hasOwnProperty(room)) {
-      roomsList.push(room);
-    }
-  }
-  return roomsList;
+
+
+var roomConstants = {
+  "General chat": true,
+  "Node course chat": true,
+  "Not safe for work": true
 };
 
 
-
-
-
-
-
+var getRoomsList = (rooms) => {
+  // var roomsList = [];
+  var roomsList = Object.keys(roomConstants);
+  
+  // for(room in rooms) {
+  //   if(!rooms[room].sockets.hasOwnProperty(room)) {
+  //     roomsList.push(room);
+  //   }
+  // }
+  
+  for(room in rooms) {
+    if(!roomConstants.hasOwnProperty(room) && !rooms[room].sockets.hasOwnProperty(room)) {
+      roomsList.push(room);
+    }
+  }
+  
+  return roomsList;
+};
 
 module.exports = {getRoomsList};
